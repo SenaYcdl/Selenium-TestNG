09737893db2481cee202a19fafd3f0a1f431c156
Sena Yucedal <sena.ycdl61@gmail.com>
2022-10-16 13:09:49 +0300

    V1

--- src/main/java/tests/day21/C01_ScreenShotReusableMethod.java
+++ src/main/java/tests/day21/C01_ScreenShotReusableMethod.java
@@ -1,25 +1,31 @@
 package tests.day21;
 
 import Pages.AmazonPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
-
 import java.io.IOException;
 
 public class C01_ScreenShotReusableMethod {
 
+    private static Logger logger = LogManager.getLogger(C01_ScreenShotReusableMethod.class.getName());
+
     @Test
     public void test01() throws IOException {
 
+        logger.info("Hepsi Burada sitesine gidiyor");
         //hepsiburada
         Driver.getDriver().get("https://hepsiburada.com");
 
+        logger.info("Ekran goruntusu aliyor");
         //ve sayfanin resmini aliniz
         ReusableMethods.getScreenshot("hepsiburada");
 
+        logger.warn("Driver kapatilir");
         //sayfayi kapatin
         Driver.closeDriver();
 
@@ -33,11 +39,11 @@ public class C01_ScreenShotReusableMethod {
         Driver.getDriver().get(ConfigReader.getProperty("amznUrl"));
 
         //nutella aratiniz
-        AmazonPage amazonPage=new AmazonPage();
+        AmazonPage amazonPage = new AmazonPage();
         amazonPage.aramaKutusu.sendKeys("Nutella", Keys.ENTER);
 
         //arama sonuc yazisini Webelementinin resmini aliniz
-        ReusableMethods.getScreenshotWebElement("aramaSonucWE",amazonPage.aramaSonucElementi);
+        ReusableMethods.getScreenshotWebElement("aramaSonucWE", amazonPage.aramaSonucElementi);
 
         //sayfayi kapatin
         Driver.closeDriver();
--- src/main/java/tests/log4J/Log4jTest2.java
+++ src/main/java/tests/log4J/Log4jTest2.java
@@ -1,15 +1,15 @@
 package tests.log4J;
 
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class Log4jTest1 {
-
-    private static Logger logger = LogManager.getLogger(Log4jTest1.class.getName());
+public class Log4jTest2 {
+    private static Logger logger = LogManager.getLogger(Log4jTest2.class.getName());
 
     @Test
-    public void log4jTest1(){
+    public void log4jTest1() {
         logger.info("LOG INFO");
         logger.debug("LOG DEBUG");
         logger.error("LOG ERROR");
